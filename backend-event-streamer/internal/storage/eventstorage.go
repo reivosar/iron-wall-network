df6e6877846bd1287a69e1ea1bde4f533ec0b687
reivosar <{ID}+{username}@users.noreply.github.com>
2024-11-17 11:19:29 +0900

    fixed bug

--- backend-event-streamer/internal/storage/eventstorage.go
+++ backend-event-streamer/internal/storage/eventstorage.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"time"
 )
 
 type Event struct {
@@ -90,10 +91,33 @@ func RecordProcessedEvent(eventID string) error {
 		return fmt.Errorf("could not connect to database: %w", err)
 	}
 
-	query := `INSERT INTO processed_events (event_id) VALUES ($1)`
-	_, err = db.Exec(context.Background(), query, eventID)
+	now := time.Now()
+
+	tx, err := db.Begin(context.Background())
 	if err != nil {
+		return fmt.Errorf("could not begin transaction: %w", err)
+	}
+
+	insertProcessedEventQuery := `
+		INSERT INTO processed_events (event_id) VALUES ($1)
+	`
+	_, err = tx.Exec(context.Background(), insertProcessedEventQuery, eventID)
+	if err != nil {
+		tx.Rollback(context.Background())
 		return fmt.Errorf("failed to insert into processed_events: %v", err)
+	}
+
+	updateActiveEventQuery := `
+		UPDATE active_events SET processing_finished_at = $1 WHERE event_id = $2
+	`
+	_, err = tx.Exec(context.Background(), updateActiveEventQuery, now, eventID)
+	if err != nil {
+		tx.Rollback(context.Background())
+		return fmt.Errorf("failed to update active_events: %v", err)
+	}
+
+	if err := tx.Commit(context.Background()); err != nil {
+		return fmt.Errorf("failed to commit transaction: %v", err)
 	}
 
 	return nil
@@ -105,10 +129,33 @@ func RecordFailedEvent(eventID string, failureReason string) error {
 		return fmt.Errorf("could not connect to database: %w", err)
 	}
 
-	query := `INSERT INTO failed_events (event_id, failure_reason) VALUES ($1, $2)`
-	_, err = db.Exec(context.Background(), query, eventID, failureReason)
+	now := time.Now()
+
+	tx, err := db.Begin(context.Background())
 	if err != nil {
+		return fmt.Errorf("could not begin transaction: %w", err)
+	}
+
+	insertFailedEventQuery := `
+		INSERT INTO failed_events (event_id, failure_reason) VALUES ($1, $2)
+	`
+	_, err = tx.Exec(context.Background(), insertFailedEventQuery, eventID, failureReason)
+	if err != nil {
+		tx.Rollback(context.Background())
 		return fmt.Errorf("failed to insert into failed_events: %v", err)
+	}
+
+	updateActiveEventQuery := `
+		UPDATE active_events SET processing_finished_at = $1 WHERE event_id = $2
+	`
+	_, err = tx.Exec(context.Background(), updateActiveEventQuery, now, eventID)
+	if err != nil {
+		tx.Rollback(context.Background())
+		return fmt.Errorf("failed to update active_events: %v", err)
+	}
+
+	if err := tx.Commit(context.Background()); err != nil {
+		return fmt.Errorf("failed to commit transaction: %v", err)
 	}
 
 	return nil
